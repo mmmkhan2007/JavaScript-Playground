135e536b609320b3df31562e96a8ec8c275f6a6a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-30 14:23:46 +0200

    update

---
+++ 1,500+ JavaScript Practice Challenges/Flip the Boolean.js
@@ -0,0 +1,57 @@
+
+
+
+
+/*
+Flip the Boolean
+Booleans can also be written as integers, where 1 = True and 0 = False. Make a function that returns the opposite of the boolean given.
+
+Examples
+flipBool(true) ➞ 0
+
+flipBool(false) ➞ 1
+
+flipBool(1) ➞ 0
+
+flipBool(0) ➞ 1
+
+
+
+
+*/
+
+
+const flipBool=b=> b ? 0 : 1;
+
+
+
+function flipBool(b) {
+	return (!b)*1;
+}
+
+
+
+function flipBool(b) {
+	return b === true || b === 1 ? 0: 1;
+}
+
+
+
+
+flipBool = b => !b + 0
+
+
+
+function flipBool(b) {
+	return (b)? 0:1;
+}
+
+
+
+
+function flipBool(b) {
+    return b === true || b === 1 ? 0 : 1;
+  }
+
+
+
---
+++ 1,500+ JavaScript Practice Challenges/Reverse an Array.js
@@ -0,0 +1,69 @@
+
+
+
+
+/*
+
+
+Reverse an Array
+Write a function to reverse an array.
+
+Examples
+reverse([1, 2, 3, 4]) ➞ [4, 3, 2, 1]
+
+reverse([9, 9, 2, 3, 4]) ➞ [4, 3, 2, 9, 9]
+
+reverse([]) ➞ []
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+
+*/
+
+
+function reverse(arr) {
+	return arr.reverse();
+}
+
+
+
+
+
+function reverse(arr) {
+	let result = [];
+	for (let i = arr.length -1; i > -1; i--) {
+		result.push(arr[i]);
+	}
+	
+	console.log(...result);
+    return result;
+    
+
+}
+
+
+
+
+
+const reverse = arr => arr.reverse();
+
+
+
+
+
+
+
+
+function reverse(arr) {
+	let word = [];
+	for(letter of arr){
+		word.unshift(letter)
+	}
+	return word
+}
+
+
+
+
