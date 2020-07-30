08b11b35a005e760e365eaddabaff6f91bbb6f5b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-30 14:08:21 +0200

    update

---
+++ 1,500+ JavaScript Practice Challenges/Solve the Equation.js
@@ -0,0 +1,29 @@
+
+
+
+
+/*
+
+
+Solve the Equation
+Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+
+Examples
+equation("1+1") ➞ 2
+
+equation("7*4-2") ➞ 26
+
+equation("1+1+1+1+1") ➞ 5
+
+
+*/
+
+
+
+function equation(s) {
+	return eval(s);
+}
+
+
+
+const equation = eval;
