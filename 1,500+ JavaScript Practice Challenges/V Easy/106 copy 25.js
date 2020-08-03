59c900205b26560da56215d580170b01c494b214
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-03 12:43:56 +0200

    update solutions to tasks

---
+++ 1,500+ JavaScript Practice Challenges/V Easy/106 copy 25.js
@@ -0,0 +1,41 @@
+
+
+
+
+/*
+
+
+
+
+
+*/
+
+
+
+
+
+
+/*  Solution 1   */
+
+
+const arr = ["cars", "planes", ["trains", ["motorcycles"]]]
+
+// Fix the following using es6 destructuring
+// Only edit what's inside of [ trans1, trans2, trans3, trans4 ]
+let [ trans1, trans2, trans3, trans4 ] = arr.flat(Infinity)
+
+/* 
+console.log(trans1) // should output "cars"
+console.log(trans2) // should output "planes"
+console.log(trans3) // should output "trains"
+console.log(trans4) // should output "motorcycles"
+*/
+
+
+/*  Solution 2   */
+
+
+
+
+
+/*  Solution 3   */
