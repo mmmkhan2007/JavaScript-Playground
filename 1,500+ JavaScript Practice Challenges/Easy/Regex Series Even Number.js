efdd9087d73fcbd7748f51d7c8d0a108d6172c98
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-03 13:26:25 +0200

    update

---
+++ 1,500+ JavaScript Practice Challenges/Easy/Regex Series Even Number.js
@@ -0,0 +1,42 @@
+
+
+
+
+/*
+
+
+Regex Series: Even Number?
+Write a regular expression that matches only an even number. Numbers will be presented as strings.
+
+Examples
+"2341" ➞ false
+
+"132" ➞ true
+
+"29" ➞ false
+
+"5578" ➞ true
+Notes
+This challenge is designed to use Regex only.
+
+
+*/
+
+
+
+
+
+
+/*  Solution 1   */
+
+
+let x = /^\d*[02468]$/
+
+
+/*  Solution 2   */
+
+
+
+
+
+/*  Solution 3   */
