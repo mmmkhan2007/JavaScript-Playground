b12fa5fae2fa8ad47aad26c59f85a20e2d66c8a8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-07 11:05:41 +0200

    update

---
+++ 1,500+ JavaScript Practice Challenges/Medium/Is it an Object.js
@@ -0,0 +1,45 @@
+/*
+
+
+Is it an Object?
+Create a function to check whether the given parameter is an Object or not.
+
+Examples
+isObject(function add(x,y) {return x + y;}) ➞ true
+
+isObject(new Regex("^[a-zA-Z0-9]+$)","g")) ➞ true
+
+isObject(null) ➞ false
+
+isObject("") ➞ false
+Notes
+Inputs may be null, primitive wrapper types, dates.
+
+
+*/
+
+
+
+
+
+
+/*  Solution 1   */
+
+
+
+
+
+/*  Solution 2   */
+
+
+
+
+
+/*  Solution 3   */
+
+
+
+
+
+
+/*  Solution 4   */
