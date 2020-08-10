5cefc564e2c6cb5d5192a484c85f290f80cb7316
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-10 10:00:23 +0200

    udpate edabit challenges

---
+++ 1,500+ JavaScript Practice Challenges/Medium/Double Letters.js
@@ -0,0 +1,44 @@
+/*
+
+
+
+Double Letters
+Create a function that takes a word and returns true if the word has two consecutive identical letters.
+
+Examples
+doubleLetters("loop") ➞ true
+
+doubleLetters("yummy") ➞ true
+
+doubleLetters("orange") ➞ false
+
+doubleLetters("munchkin") ➞ false
+Notes
+
+*/
+
+
+
+
+
+
+/*  Solution 1   */
+
+
+
+
+
+/*  Solution 2   */
+
+
+
+
+
+/*  Solution 3   */
+
+
+
+
+
+
+/*  Solution 4   */
---
+++ 1,500+ JavaScript Practice Challenges/Medium/Xs and Os, Nobody Knows.js
@@ -0,0 +1,51 @@
+/*
+
+Xs and Os, Nobody Knows
+Create a function that takes a string, checks if it has the same number of x's and o's and returns either true or false.
+
+Return a boolean value (true or false).
+The string can contain any character.
+When no x and no o are in the string, return true.
+Examples
+XO("ooxx") ➞ true
+
+XO("xooxx") ➞ false
+
+XO("ooxXm") ➞ true
+// Case insensitive.
+
+XO("zpzpzpp") ➞ true
+// Returns true if no x and o.
+
+XO("zzoo") ➞ false
+Notes
+Remember to return true if there aren't any x's or o's.
+Must be case insensitive.
+
+*/
+
+
+
+
+
+
+/*  Solution 1   */
+
+
+
+
+
+/*  Solution 2   */
+
+
+
+
+
+/*  Solution 3   */
+
+
+
+
+
+
+/*  Solution 4   */
