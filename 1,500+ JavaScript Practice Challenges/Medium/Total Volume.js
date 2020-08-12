bffe043fe4e49fe899f09b0d18dd55b8dc1d774f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-12 13:05:33 +0200

    update

--- 1,500+ JavaScript Practice Challenges/Medium/Total Volume.js
+++ 1,500+ JavaScript Practice Challenges/Medium/Total Volume.js
@@ -26,19 +26,83 @@ Each box will always have three dimensions included.
 
 /*  Solution 1   */
 
+const totalVolume = (...boxes) => boxes.reduce((total, [l, w, h]) => total + l * w * h, 0);
+
+const totalVolume = (...boxes) => boxes.reduce((a, b) => a + b.reduce((c, d) => c * d, 1), 0);
+
+totalVolume = (...a) => a.reduce((a, b) => a + b.reduce((c, d) => c * d), 0);
+
+const totalVolume = (...boxes) => boxes.map(el => el.reduce((sum, item) => sum *= item, 1)).reduce((a, b) => a + b, 0)
+
+
+
 
 
 /*  Solution 2   */
 
 
-
+function totalVolume(...boxes) {
+    return [...boxes].map(arr => arr.reduce((a, v) => a * v)).reduce((a, v) => a + v)
+}
 
 
 /*  Solution 3   */
 
-
+function totalVolume(...boxes) {
+    return boxes.map(x => x.reduce((a, b) => a * b)).reduce((a, b) => a + b);
+}
 
 
 
 
 /*  Solution 4   */
+
+
+
+
+function totalVolume(...boxes) {
+    var sum = 0;
+    for (i = 0; i < boxes.length; i++) {
+        sum += boxes[i][0] * boxes[i][1] * boxes[i][2];
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+function totalVolume(...boxes) {
+    let total = 0;
+    for (var i = 0; i < boxes.length; i++) {
+        total += boxes[i].reduce((a, b) => a * b);
+    }
+    return total;
+}
+
+
+
+function totalVolume(...boxes) {
+    let total = 0;
+
+    for (i = 0; i < boxes.length; i++) {
+        let temp = boxes[i]
+        let multiple = 1;
+        for (x = 0; x < temp.length; x++) {
+            multiple = temp[x] * multiple
+        }
+        total += multiple
+    }
+    return total
+}
+
+
+
+
+
+
+function totalVolume(...boxes) {
+    return [...boxes].map(arr => arr.reduce((a, v) => a * v)).reduce((a, v) => a + v)
+}
