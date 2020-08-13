b438eabbc7d6107fee5034755388feecce49aa38
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-13 14:02:50 +0200

    create edabit solutions

---
+++ 1,500+ JavaScript Practice Challenges/V Easy/Nth Star Number.js
@@ -0,0 +1,28 @@
+/*  
+
+Nth Star Number
+Create a function that takes a positive integer and returns the nth "star number".
+
+A star number is a centered figurate number a centered hexagram (six-pointed star), such as the one that Chinese checkers is played on.
+
+Examples
+starNumber(2) ➞ 13
+
+starNumber(3) ➞ 37
+
+starNumber(5) ➞ 121
+Notes
+See Resources for more information.
+*/
+
+
+
+
+
+
+
+/*  
+
+Solutions
+
+*/
