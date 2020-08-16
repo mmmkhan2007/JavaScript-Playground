8dce0898c109cc7a4dc9bc579823c26b3f780445
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 11:49:36 +0200

    update

--- hackerrank.com/10 Days of Javascript/Day 6/Bitwise Operators.js
+++ hackerrank.com/10 Days of Javascript/Day 6/Bitwise Operators.js
@@ -4,6 +4,38 @@
  *	n: Set S is a sequence of distinct integers from 1 to n (i.e., {1, 2, ..., n}).
  *	k: An integer.
  */
+
+
+
+//online refernece used
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+
+
 function getMaxLessThanK(n, k) {
     let maximum = 0;
     let current = -1;
@@ -17,4 +49,15 @@ function getMaxLessThanK(n, k) {
         }
     }
     return maximum;
+}
+
+
+function main() {
+    const q = +(readLine());
+
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+
+        console.log(getMaxLessThanK(n, k));
+    }
 }
