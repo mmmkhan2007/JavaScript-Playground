38aad3b7727f55887e35f6a5315963be7e284908
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 12:00:15 +0200

    add Event Listeners

--- Pure JavaScript Mini Projects/Todo-List/temp.js
+++ Pure JavaScript Mini Projects/Todo-List/temp.js
@@ -7,3 +7,9 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const todoFilter = document.querySelector('.filter-todo');
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', populateTodos);
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteOrCheck);
+todoFilter.addEventListener('change', showFilteredTodos);
