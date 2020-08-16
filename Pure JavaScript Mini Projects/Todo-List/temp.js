1f24ad930fa3c533ed1a28d8af23f2a15b2573ff
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 12:00:48 +0200

    add Front-end Functions

--- Pure JavaScript Mini Projects/Todo-List/temp.js
+++ Pure JavaScript Mini Projects/Todo-List/temp.js
@@ -13,3 +13,103 @@ document.addEventListener('DOMContentLoaded', populateTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteOrCheck);
 todoFilter.addEventListener('change', showFilteredTodos);
+
+
+// Front-end Functions
+
+// shows any todos that were saved in the local storage
+function populateTodos() {
+    for (let element of todosArr) {
+        todoList.appendChild(element);
+    }
+    showFilteredTodos();
+}
+
+function addTodo(event) {
+    // prevent form from submitting
+    event.preventDefault();
+
+    if (todoInput.value.trim() == '')
+        return;
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const todoListItem = document.createElement('li');
+    todoListItem.innerHTML = todoInput.value;
+    todoListItem.classList.add('todo-item');
+
+    todoInput.value = '';
+
+    const todoCheckedButton = document.createElement('button');
+    todoCheckedButton.innerHTML = '<i class="fas fa-check"></i>';
+    todoCheckedButton.classList.add('complete-btn');
+
+    const todoDeleteButton = document.createElement('button');
+    todoDeleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDeleteButton.classList.add('delete-btn');
+
+    todoDiv.appendChild(todoListItem);
+    todoDiv.appendChild(todoCheckedButton);
+    todoDiv.appendChild(todoDeleteButton);
+
+    todoList.appendChild(todoDiv);
+    showFilteredTodos();
+
+    // store the newly created todo item
+    saveTodo(todoDiv);
+}
+
+function deleteOrCheck(event) {
+    const element = event.target;
+    if (element.classList[0] == 'delete-btn') {
+        // delete the parent div (the whole todo div)
+        // adding transformation
+        element.parentElement.classList.add('fall');
+        // waiting until the transaction ends then removing the parent div
+        element.parentElement.addEventListener('transitionend', function() {
+            // delete the todo item
+            // from local storage
+            element.parentElement.classList.remove('fall');
+            deleteTodo(element.parentElement);
+            // and from the DOM
+            element.parentElement.remove();
+        });
+    } else if (element.classList[0] == 'complete-btn') {
+        // mark as completed
+        // const completedColor = 'yellowgreen';
+        // const parent = element.parentElement;
+        // const currentBackground = parent.style.background;
+        // parent.style.background = (currentBackground != completedColor)?
+        //                            completedColor : 'white';
+        element.parentElement.classList.toggle('completed');
+        showFilteredTodos();
+
+        // update the todo item
+        updateTodo(element.parentElement);
+    }
+}
+
+function showFilteredTodos(event) {
+    const type = todoFilter.value;
+    let elements = todoList.children;
+
+    for (let element of elements) {
+        let shouldBeShown = true;
+        let hasCompletedClass = element.classList.contains('completed');
+
+        switch (type) {
+            case 'completed':
+                shouldBeShown = hasCompletedClass;
+                break;
+            case 'incompleted':
+                shouldBeShown = !hasCompletedClass;
+        }
+
+        if (shouldBeShown)
+            element.classList.remove('hide');
+        else
+            element.classList.add('hide');
+    }
+
+}
