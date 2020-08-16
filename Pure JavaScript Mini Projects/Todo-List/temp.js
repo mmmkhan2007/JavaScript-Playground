831230bf98df353fc80160cf49d38d0c8f12340f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 11:57:10 +0200

    add header, form, body styles

---
+++ Pure JavaScript Mini Projects/Todo-List/temp.js
@@ -0,0 +1,23 @@
+* {
+    margin: 0;
+    padding: 0;
+    box - sizing: border - box;
+}
+
+body {
+    background - image: linear - gradient(120 deg, #f6d365, #d88771);
+    color: rgb(255, 238, 238);
+    font - family: 'Poppins', sans - serif;
+    min - height: 100 vh;
+}
+
+header {
+    font - size: 1.5 rem;
+}
+
+header, form {
+    min - height: 20 vh;
+    display: flex;
+    justify - content: center;
+    align - items: center;
+}
