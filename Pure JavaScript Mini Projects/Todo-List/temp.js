336665809833b07f3b0f4ca91f60f48eb9748ea6
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 12:00:03 +0200

    add Selectors

--- Pure JavaScript Mini Projects/Todo-List/temp.js
+++ Pure JavaScript Mini Projects/Todo-List/temp.js
@@ -1,3 +1,9 @@
 // Objects
 const domParser = new DOMParser();
 const todosArr = loadAllTodosFromLocalStorage();
+
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const todoFilter = document.querySelector('.filter-todo');
