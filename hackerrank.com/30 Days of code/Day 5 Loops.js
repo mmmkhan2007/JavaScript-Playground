28f7dc37c9c83b1dfe2b3563d9050fc006c9a416
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-18 10:04:04 +0200

    add day 7 - Arrays

--- hackerrank.com/30 Days of code/Day 5 Loops.js
+++ hackerrank.com/30 Days of code/Day 5 Loops.js
@@ -29,3 +29,17 @@ function main() {
         console.log(`${n} x ${i} = ${i*n}`)
     }
 }
+
+
+
+
+
+
+
+
+function main() {
+    var n = parseInt(readLine());
+    for (i = 1; i <= 10; i++) {
+        console.log(n + ' x ' + i + ' = ' + (n * i))
+    }
+}
--- hackerrank.com/30 Days of code/Day 6 Let's Review.js
+++ hackerrank.com/30 Days of code/Day 6 Let's Review.js
@@ -29,3 +29,36 @@ function processData(input) {
     input = input.split `\n`.slice(1)
     input.map(v => console.log(even([...v]).join ``, odd([...v]).join ``))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function processData(input) {
+    //Enter your code here
+    input = input.split('\n');
+
+    for (i = 1; i < input.length; i++) {
+        var splitWord = input[i].split('');
+
+        var even = '';
+        var odd = '';
+
+        for (x = 0; x < splitWord.length; x++) {
+            if (x % 2 === 0) {
+                even = even + splitWord[x];
+            } else {
+                odd = odd + splitWord[x];
+            }
+        }
+        console.log(even + ' ' + odd);
+    }
+}
