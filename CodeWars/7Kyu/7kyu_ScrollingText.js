9529853a96df5aaad45427d557a13a1143dea98d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-19 09:29:07 +0200

    Create 7kyu_ScrollingText.js

---
+++ CodeWars/7Kyu/7kyu_ScrollingText.js
@@ -0,0 +1,18 @@
+// 7kyu - Scrolling Text
+
+// Let's create some scrolling text!
+
+// Your task here is to write the function scrollingText, which takes a string, 
+// and returns an array:
+
+// scrollingText("codewars") should return
+
+// ["CODEWARS",
+// "ODEWARSC",
+// "DEWARSCO",
+// "EWARSCOD",
+// "WARSCODE",
+// "ARSCODEW"
+// "RSCODEWA",
+// "SCODEWAR"]
+// Good luck!
