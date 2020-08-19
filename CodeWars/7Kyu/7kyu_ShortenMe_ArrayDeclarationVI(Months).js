e9e157344a98fc2eedc8ee402c13b587f46abfd9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-19 09:32:59 +0200

    7kyu_ShortenMe_ArrayDeclarationVI(Months).js

---
+++ CodeWars/7Kyu/7kyu_ShortenMe_ArrayDeclarationVI(Months).js
@@ -0,0 +1,11 @@
+// 7kyu - Shorten Me: Array Declaration VI (Months)
+// Task
+// You have to write an array() function returning an array object with elements 
+// as follow:
+
+// [ 'January', 'February', 'March', 'April', 'May', 'June',
+//   'July', 'August', 'September', 'October', 'November', 'December',
+//   'January', 'February', 'March', 'April', 'May', 'June',
+//   'July', 'August', 'September', 'October', 'November', 'December' ]
+// Coding Limitation:
+// Less than 120 characters
