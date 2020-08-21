d715cca9e58c7eb97632d28cf2f75478f0d5a748
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-21 11:14:40 +0200

    update solutions to Kyu8

--- hackerrank.com/Easy/Between Two Sets.js
+++ hackerrank.com/Easy/Between Two Sets.js
@@ -27,6 +27,17 @@ Explanation
 4, 8 and 16 divide evenly into 16, 32, 96.
 4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.
 */
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
 function getTotalX(a, b) {
     let sortedA = a.sort();
     let sortedB = b.sort();
