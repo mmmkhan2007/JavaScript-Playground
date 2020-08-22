a5d71ae7e09c734f7581f41a521926b2819808b3
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-22 17:46:05 +0200

    add solutions to LeetCode Easy part challenges

---
+++ LeetCode/Easy/dist/119-pascalTriangleII.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * calculate the element in reverse, resultRow[i] += resultRow[i-1]
+ * we don't have to keep resultRow[i] if we do it from back to start.
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function getRow(rowIndex) {
+  var resultRow = [1];
+
+  for (var i = 1; i <= rowIndex; i++) {
+    for (var j = i - 1; j > 0; j--) {
+      resultRow[j] += resultRow[j - 1];
+    }
+
+    resultRow.push(1);
+  }
+
+  return resultRow;
+};
