555b38eda777939a6a56abcd53c14772b554acde
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-23 15:11:24 +0200

    Added easy/dash_insert

---
+++ CoderByte/easy/dist/dash_insert.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Have the function dashInsert(str) insert dashes ('-') between each two odd
+ * numbers in str. For example: if str is 454793 the output should be 4547-9-3.
+ * Don't count zero as an odd number.
+ *
+ * https://www.coderbyte.com/results/bhanson:Dash%20Insert:JavaScript
+ *
+ * @param  {string} str
+ * @return {string}
+ */
+function dashInsert(str) {
+  var newString = str[0] || '';
+
+  for (var i = 1, lastNumberIsOdd = str[0] % 2 === 1; i < str.length; i++) {
+    var numberIsOdd = str[i] % 2 === 1;
+    newString += numberIsOdd && lastNumberIsOdd ? '-' + str[i] : str[i];
+    lastNumberIsOdd = numberIsOdd;
+  }
+
+  return newString;
+}
+
+module.exports = dashInsert;
