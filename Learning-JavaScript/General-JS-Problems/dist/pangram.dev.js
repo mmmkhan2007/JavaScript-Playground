29c7f8c1f48a01f7036106d52fceb1b51dd22e02
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-25 17:11:32 +0200

    update GeneralJS problems

---
+++ Learning-JavaScript/General-JS-Problems/dist/pangram.dev.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*Pangrams are sentences constructed by using every letter of the alphabet at least once. Construct a function to check if a string is a Panagram.
+Input Format: Input consists of a string ss.
+Length of ss can be at most 103103 (1≤|s|≤103)(1≤|s|≤103) and it may contain spaces, 
+lower case and upper case letters. Lower-case and upper-case instances of a letter are considered the same.
+Output: Output a line containing pangram if ss is a pangram, otherwise output not pangram.
+*/
+
+/*Solutio Algo - 
+A) Build a hash-map where each alphabets (A to Z i.e. from charCode 65 to charCode 90) are key and their value is set to false.
+B> Then from the given sting in the argument, push each of its alphabets to the Hash as the key and set its value to true. If the given string is a panagram, then after this operation the Hash should have all its key's value to be set to true.
+C> Then iterate throught the Hash's key, and if I find any key's value to be false, then return false.
+*/
+isPangram = function isPangram(str) {
+  var alphabets = {};
+  str = str.replace(/ /g, '').toLowerCase();
+
+  for (var i = 65; i <= 90; i++) {
+    alphabets[String.fromCharCode(i)] = false;
+  }
+
+  if (str.length < 26) {
+    return false;
+  } else {
+    for (var j = 0; j < str.length; j++) {
+      alphabets[str[j]] = true;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog")); // should return true
---
+++ Learning-JavaScript/General-JS-Problems/dist/parenthesis-match.dev.js
@@ -0,0 +1,190 @@
+"use strict";
+
+function _toArray(arr) { return _arrayWithHoles(arr) || _iterableToArray(arr) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+/* Write a 'balancedParenthesis' function that takes a string as input and returns a boolean — if the parentheses in the input string are 'balanced', then return true, else return false
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or { ) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type.
+Algorithm:
+1) Declare a character stack S.
+2) Now traverse the expression string exp.
+    a) If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+    b) If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped character is the matching starting bracket then fine else parenthesis are not balanced.
+3) After complete traversal, if there is some starting bracket left in stack then “not balanced”
+*/
+//Alternate-1
+
+/*
+– Declare a hash-map with opening and closing braces
+– Start on the first character and repeat for each character
+– If that character is an opening brace add it to a stack
+– If that character is a closing brace pop from the stack
+– If the popped brace matches the current brace then continue with next iteration of the for loop
+– If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+– At the end if the stack is not empty then fail  */
+var isMatchingBrackets = function isMatchingBrackets(str) {
+  var stack = [];
+  var map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  for (var i = 0; i < str.length; i++) {
+    // If character is an opening brace add it to a stack
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+      stack.push(str[i]);
+    } //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+    else {
+        var last = stack.pop(); //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+
+        if (str[i] !== map[last]) {
+          return false;
+        }
+
+        ;
+      }
+  } // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
+
+
+  if (stack.length !== 0) {
+    return false;
+  }
+
+  ;
+  return true;
+}; // console.log(isMatchingBrackets("(){}")); // returns true
+// console.log(isMatchingBrackets("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]")); // returns true
+// console.log(isMatchingBrackets("({(()))}}"));  // returns false
+//Alternate-2
+
+
+var isParenthesisMatching = function isParenthesisMatching(str) {
+  var stack = [];
+  var open = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  };
+  var closed = {
+    '}': true,
+    ']': true,
+    ')': true
+  };
+
+  for (var i = 0; i < str.length; i++) {
+    var _char = str[i];
+
+    if (open[_char]) {
+      stack.push(_char);
+    } else if (closed[_char]) {
+      if (open[stack.pop()] !== _char) return false;
+    }
+  }
+
+  return stack.length === 0;
+}; // console.log(isParenthesisMatching("(){}"));
+// console.log(isParenthesisMatching("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]"));
+
+/* If a closed chr is found, check if the matching closed parenthesis of the last element that is popped off the stack (the last open parenthesis symbol found in the string) is not equal to the current chr
+if the chr isn’t the matching closed parenthesis for the last open parenthesis from the stack, then we return false because we have an imbalanced input.
+Time Complexity -
+Our solution iterates the length of the input string, meaning that our time cost will grow in linear proportion to the growth of the string length. Or, for each character that is added to the input string, the algorithm will take 1 time unit longer to complete (worst case). All other operations in our algorithm are constant time because we are using object-property lookup to find our comparison values and the pop method of a Stack data structure to keep track of all the parenthesis pairs that get opened.
+ */
+
+/* Parenthesis matching with Array.reduce() method
+Basically, this function just increments the variable uptoPrevChar for each opening parenthesis and reduces it for each closing parenthesis. And ultimately I should just get zero for a balance string.
+However, I have to return a boolean output - so I put a logical not ( ! ) at the very front of str. So for numerical return value of 0 for variable 'uptoPrevChar' - I return true.
+So for the below given test-case, the first iteration of the function will return 4, as there are 4 opening brackets. And then it will continue to decrement it and finally return zero.
+Just paste this code, in Chrome DevTool > Source > Snippets > Put a breakpoint at the return value of 'uptoPrevChar' on the second else if iteration > right click on the snippets and run > The continue clicking on 'resume script execution'. NOTE - in Chrome Dev-Tool, use 'var' instead of 'let'
+*/
+
+
+var isBalancedParenthesis = function isBalancedParenthesis(str) {
+  return !str.split('').reduce(function (uptoPrevChar, thisChar) {
+    if (thisChar === '(' || thisChar === '{' || thisChar === '[') {
+      return ++uptoPrevChar;
+    } else if (thisChar === ')' || thisChar === '}' || thisChar === ']') {
+      return --uptoPrevChar;
+    }
+
+    return uptoPrevChar;
+  }, 0);
+}; // Test Cases
+// console.log(isBalancedParenthesis("[()]{}{[()()]()}"));  // returns true
+// console.log(isBalancedParenthesis("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]"));  // returns true
+// console.log(isBalancedParenthesis("({(()))}}"));  // returns false
+// Solving with reduce() and using ES6
+
+
+function isBalanced(_ref) {
+  var _ref2 = _toArray(_ref),
+      str = _ref2.slice(0);
+
+  return str.reduce(function (uptoPrevChar, thisChar) {
+    (thisChar === '(' && uptoPrevChar++ || thisChar === ')' && uptoPrevChar--) && (thisChar === '{' && uptoPrevChar++ || thisChar === '}' && uptoPrevChar--) && (thisChar === '[' && uptoPrevChar++ || thisChar === ']' && uptoPrevChar--);
+    return uptoPrevChar;
+  }, 0) === 0;
+} // Test Cases
+// console.log(isBalanced("[()]{}{[()()]()}"));  // returns true
+// console.log(isBalanced("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]"));  // returns true
+// console.log(isBalanced("({(()))}"));
+
+/* ALTERNATIVE - 3
+PROBLEM STATEMENT - isBalancedBraces - Takes a string and returns true or false indicating whether its curly braces are balanced.
+isBalanced('}{')                      // false
+isBalanced('{{}')                     // false
+isBalanced('{}{}')                    // true
+isBalanced('foo { bar { baz } boo }') // true
+isBalanced('foo { bar { baz }')       // false
+isBalanced('foo { bar } }')
+*/
+
+
+isBalancedBraces = function isBalancedBraces(str) {
+  var count = 0;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = str[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var i = _step.value;
+      if (i === "{") count++;else if (i === "}") count--; // Noting from the first test-case it looks like that if a closing curly brace is first encountered, then my function should return 'false'. So as soon as I have a negative value of count, I should return false.
+
+      if (count < 0) return false;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return count === 0;
+};
+
+console.log(isBalancedBraces('}{')); // false
+
+console.log(isBalancedBraces('{{}')); // false
+
+console.log(isBalancedBraces('{}{}')); // true
+
+console.log(isBalancedBraces('foo { bar { baz } boo }')); // true
+
+console.log(isBalancedBraces('foo { bar { baz }')); // false
+
+console.log(isBalancedBraces('foo { bar } }')); // false
