c138c043f4ba17eb838a7dedd396791594f3b635
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-25 15:02:05 +0200

    add solutions to kyu8

---
+++ CodeWars/8Kyu/dist/ReturnToSanity.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// 8kyu - Return to Sanity
+// This function returns something strange. What's wrong?
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+function mystery() {
+  return {
+    sanity: 'Hello'
+  };
+}
