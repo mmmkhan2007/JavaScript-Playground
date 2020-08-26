4bb5c438d04742470ba38cb60e407511d7b8d529
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-26 10:49:49 +0200

    add solutions to LeetCode challegnes

---
+++ LeetCode/Easy/dist/202-happyNumber.dev.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function isHappy(n) {
+  var squareSums = [n];
+
+  while (n !== 1) {
+    n = squareSum(n); // check this square sum num exists or not.
+
+    var isSquareSumApeared = squareSums.indexOf(n) > -1;
+    if (isSquareSumApeared) return false;
+    squareSums.push(n);
+  }
+
+  return true;
+};
+
+var squareSum = function squareSum(n) {
+  var sum = 0;
+
+  while (n > 0) {
+    var lastDigit = n % 10;
+    sum += lastDigit * lastDigit;
+    n = Math.floor(n / 10);
+  }
+
+  return sum;
+};
