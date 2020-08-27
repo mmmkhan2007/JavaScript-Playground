9ec18944ee82579c81d66fe39e92a55c233ec541
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-27 20:16:01 +0200

    add solutions to LeetCode/easy challenges

---
+++ LeetCode/Easy/dist/228-summaryRanges.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function summaryRanges(nums) {
+  var ranges = [];
+
+  for (var i = 0; i < nums.length; i++) {
+    var j = i;
+
+    while (nums[j + 1]) {
+      if (nums[j + 1] - nums[j] === 1) j++;else break;
+    }
+
+    if (nums[i] !== nums[j]) {
+      ranges.push(nums[i] + '->' + nums[j]);
+    } else {
+      ranges.push('' + nums[i]);
+    }
+
+    i = j;
+  }
+
+  return ranges;
+};
