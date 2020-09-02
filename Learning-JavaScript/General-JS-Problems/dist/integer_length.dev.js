74881ce4e05510c7451cff91199fd9f4d678d18a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-02 16:28:26 +0200

    added random-code-1.js

--- Learning-JavaScript/General-JS-Problems/dist/integer_length.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-/* Write a function that takes an integer as input and returns the number of digits in that integer.*/
-function integerLength(num) {
-  return ('' + (num | 0)).length;
-}
-
-console.log(integerLength());
