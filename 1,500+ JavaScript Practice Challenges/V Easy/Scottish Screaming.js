06ae62239b0f112f9375cc359b414b3a4f988f03
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-10-03 15:04:46 +0200

    added solutions to edabit/ easy 'String and Number Conversions'

--- 1,500+ JavaScript Practice Challenges/V Easy/Scottish Screaming.js
+++ 1,500+ JavaScript Practice Challenges/V Easy/Scottish Screaming.js
@@ -1,6 +1,21 @@
 /*  
 
+Scottish Screaming
 
+A strong Scottish accent makes every vowel similar to an "e", so you should replace every vowel with an "e". Additionally, it is being screamed, so it should be in block capitals.
+
+Create a function that takes a string and returns a string.
+
+Examples
+toScottishScreaming("hello world") ➞ "HELLE WERLD"
+
+toScottishScreaming("Mr. Fox was very naughty") ➞ "MR. FEX WES VERY NEEGHTY"
+
+toScottishScreaming("Butterflies are beautiful!") ➞ "BETTERFLEES ERE BEEETEFEL!"
+
+Notes
+Make sure to include all punctuation that is in the original string.
+You don't need any more namespaces than are already given.
 
 */
 
@@ -20,7 +35,10 @@
 //#############################################################
 
 
+const toScottishScreaming = s => s.toUpperCase().replace(/[AIOU]/g, 'E');
 
+
+const toScottishScreaming = str => str.replace(/[aeiou]/gi, 'E').toUpperCase();
 
 
 
@@ -30,7 +48,9 @@
 //#############################################################
 
 
-
+const toScottishScreaming = str => (
+    str.toUpperCase().replace(/[AIOU]/g, 'E')
+);
 
 
 
@@ -39,6 +59,20 @@
 //#############################################################
 //#    SOLUTION 3
 //#############################################################
+
+
+function toScottishScreaming(str) {
+
+    let vowels = /[aeiou]/i;
+
+    str = str.split("");
+
+    for (let i in str) {
+        if (vowels.test(str[i])) str[i] = "E";
+    }
+
+    return str.join("").toUpperCase();
+}
 
 
 
@@ -51,13 +85,8 @@
 //#############################################################
 
 
+function toScottishScreaming(str) {
+    const regex = /[aeiouAEIOUA]/g;
 
-
-
-
-
-
-
-//#############################################################
-//#    SOLUTION 5
-//#############################################################
+    return str.replace(regex, 'e').toUpperCase();
+}
